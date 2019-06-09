71208ff6d2dce3bb55467ecebbc3658900351a28
Sheil Naik <sheil@Sheil--Julies-MacBook-Pro.local>
2019-06-09 00:40:41 -0400

    Add check for banned company

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
 import mysql.connector
 import os
@@ -8,8 +8,8 @@ app = Flask(__name__)
 api = Api(app)
 
 
-@app.route('/company/violation_count/<string:company_name>')
-def get(company_name):
+@app.route('/violation_count')
+def violation_count():
     db = mysql.connector.connect(
         host="easyethics.ccecgtqyo5kq.us-east-1.rds.amazonaws.com",
         user=os.environ['DB_USERNAME'],
@@ -18,7 +18,7 @@ def get(company_name):
     )
     cursor = db.cursor()
 
-    company_name = company_name.upper()
+    company_name = request.args.get('company_name', type=str).upper()
 
     query = """
     SELECT c.name, r.rating, COUNT(r.rating) as violation_count
@@ -26,7 +26,7 @@ def get(company_name):
     JOIN ratings r
     ON c.id = r.company
     WHERE UPPER(c.name) = '{company_name}'
-    GROUP BY c.name, r.rating
+    GROUP BY c.name, r.rating;
     """.format(
         company_name=company_name
     )
@@ -51,5 +51,37 @@ def get(company_name):
 
     json_result = json.dumps(api_result, sort_keys=True)
     return json_result, 200
+
+
+@app.route('/banned_company')
+def banned_company():
+    db = mysql.connector.connect(
+        host="easyethics.ccecgtqyo5kq.us-east-1.rds.amazonaws.com",
+        user=os.environ['DB_USERNAME'],
+        passwd=os.environ['DB_PASSWORD'],
+        database="easyethics"
+    )
+    cursor = db.cursor()
+
+    user_id = request.args.get('user_id', type=int)
+    company_id = request.args.get('company_id', type=int)
+
+    query = """
+    SELECT COUNT(*)
+    FROM easyethics.ratings
+    WHERE user = {user_id} and company = {company_id};
+    """.format(
+        user_id=user_id,
+        company_id=company_id
+    )
+
+    cursor.execute(query)
+
+    result = cursor.fetchall()
+    if result[0][0] > 0:
+        return 'True', 200
+    else:
+        return 'False'
+
 
 app.run(host='0.0.0.0', port=80)
